5423f8a62d540bcecb7634482596fa7ca0af20b1
vbogd <vbogd@users.noreply.github.com>
2025-02-15 19:03:32 +0300

    improve 'search' page

--- src/pages/search.py
+++ src/pages/search.py
@@ -1,5 +1,5 @@
 import dash
-from dash import html, dcc, callback, Input, Output
+from dash import html, callback, Input, Output
 import dash_bootstrap_components as dbc
 import logging
 
@@ -16,9 +16,14 @@ logger = logging.getLogger(__name__)
 def _get_calc_link(bond: dict):
     secid = bond['secid']
     shortname = bond['shortname']
-    return html.Div(
-            dcc.Link(shortname, href=f"calc/{secid}")
-        )
+    isin = bond['isin']
+    return dbc.ListGroupItem(
+        [
+            html.P(shortname, className="mb-0"),
+            html.Small(isin, className="text-muted")
+        ],
+        href=f"calc/{secid}"
+    )
 
 @callback(
     Output("isin_search_result", "children"),
@@ -26,12 +31,12 @@ def _get_calc_link(bond: dict):
 )
 def search_isin(isin: str):
     if len(isin) < 3:
-        return dbc.Label("Введите минимум 3 символа")
+        return dbc.ListGroupItem("Введите минимум 3 символа")
     logger.info(f"Search bonds for query: '{isin}' ...")
     bonds = moex_search(isin)
     logger.info(f"Search bonds for query: '{isin}' - found {len(bonds)} bond(s)")
     if len(bonds) == 0:
-        return dbc.Label("Ничего не найдено")
+        return dbc.ListGroupItem("Ничего не найдено")
     return [_get_calc_link(bond) for bond in bonds]
 
 def layout(**kwargs):
@@ -52,10 +57,14 @@ def layout(**kwargs):
                         placeholder="Название или isin",
                         persistence=True,
                         debounce=1000,
-                    )
+                        size="lg",
+                    ),
                 ),
             ],
-            className="g-1 pt-1",
+            className="g-1 pt-1 m-2",
         ),
-        html.Div(id="isin_search_result")
+        dbc.ListGroup(
+            id="isin_search_result",
+            flush=True,
+        ),
     ]
